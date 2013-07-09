1671cc509655fbd17b11ffb86bb2c68cf2caeed8
Ubuntu <ubuntu@ip-172-31-11-6.us-west-2.compute.internal>
2013-07-09 01:27:56 +0000

    debug -- using buffer

--- web.js
+++ web.js
@@ -2,13 +2,17 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var data = fs.readFileSync("index.html");
 
+//var data = fs.readFileSync("index.html");
 //console.log(data.toString());
+
+var buf = new Buffer(fs.readFileSync("index.html"));
+//console.log(buf.toString());
 
 app.get('/', function(request, response) {
   //response.send('Hello World2!');
-  response.send(data.toString());
+  //response.send(data.toString());
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
